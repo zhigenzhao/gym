4872c33af7bc6e2fc80b96f739495d29b3b3032a
mpetersen94 <markpetersen@g.harvard.edu>
2021-04-12 17:07:36 -0400

    Simplify installing dependencies for RLG environments

--- setup.py
+++ setup.py
@@ -10,13 +10,16 @@ extras = {
   'atari': ['atari_py~=0.2.0', 'opencv-python>=3.'],
   'box2d': ['box2d-py~=2.3.5'],
   'classic_control': [],
+  'drake': ['meshcat', 'tinydb', 'yaml'],
   'mujoco': ['mujoco_py>=1.50, <2.0', 'imageio'],
+  'pymunk': ['pymunk', 'opencv-python'],
   'robotics': ['mujoco_py>=1.50, <2.0', 'imageio'],
 }
 
 # Meta dependency groups.
 extras['nomujoco'] = list(set([item for name, group in extras.items() if name != 'mujoco' and name != "robotics" for item in group]))
 extras['all'] = list(set([item for group in extras.values() for item in group]))
+extras['rlg'] = list(set([item for group in [extras['drake'], extras['pymunk']] for item in group]))
 
 setup(name='gym',
       version=VERSION,
